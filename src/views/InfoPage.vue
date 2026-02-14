a83354a6d8cb7fd3c58b8e9cc38321db069fe00e
aiglesi6 <a_iglesias_12@hotmail.com>
2026-02-13 20:04:32 -0600

    Added glow

--- src/views/InfoPage.vue
+++ src/views/InfoPage.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="flox-col items center flex min-h-screen px-4 py-8 sm:py-12">
     <RouterLink
-      to="/info"
+      to="/"
       class="mt-8 text-sm text-gray-400 underline-offset-4 transition-colors duration-300 hover:text-green-400 hover:underline"
     >
       ← Back to Links
